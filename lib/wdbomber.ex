18f9cc9cf9485f8c8de5cc9ec7caa2b10e3586bb
Maksym Pugach <pugach.m@gmail.com>
2019-02-19 13:59:53 +0200

    🌅

---
+++ lib/wdbomber.ex
@@ -0,0 +1,18 @@
+defmodule Wdbomber do
+  @moduledoc """
+  Documentation for Wdbomber.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wdbomber.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/wdbomber_test.exs
@@ -0,0 +1,8 @@
+defmodule WdbomberTest do
+  use ExUnit.Case
+  doctest Wdbomber
+
+  test "greets the world" do
+    assert Wdbomber.hello() == :world
+  end
+end
