78157db557b12cfe34147e260e997681257962d4
Maksym Pugach <pugach.m@gmail.com>
2019-02-20 14:42:59 +0200

    implementation of actions count

--- lib/wdbomber/processor.ex
+++ lib/wdbomber/processor.ex
@@ -5,14 +5,13 @@ defmodule Wdbomber.Processor do
 
   @recv_timeout 1_500_000
 
-  def run_single(label, url, _actions, region) do
+  def run_single(label, url, actions, region) do
     start_time = DateTime.utc_now()
 
     with {:ok, %Response{body: session_create_body}} <- session_create(url, region),
          {:ok, %{"sessionId" => session_id, "status" => 0}} <- Jason.decode(session_create_body),
          session_url <- "#{url}/session/#{session_id}",
-         {:ok, %Response{body: session_navigate_body}} <- session_navigate(session_url, "about:blank"),
-         {:ok, %{"status" => 0}} <- Jason.decode(session_navigate_body),
+         :ok <- session_navigate_actions(session_url, "about:blank", actions),
          {:ok, %Response{body: session_destroy_body}} <- session_destroy(session_url),
          {:ok, %{"status" => 0}} <- Jason.decode(session_destroy_body) do
       seconds_spent = DateTime.diff(DateTime.utc_now(), start_time, :microsecond) / 1000_000
@@ -48,6 +47,17 @@ defmodule Wdbomber.Processor do
       hackney: [pool: :httpoison_pool]
     )
   end
+
+  defp session_navigate_actions(session_url, url_to_navigate, actions) when actions > 0 do
+    with {:ok, %Response{body: session_navigate_body}} <- session_navigate(session_url, url_to_navigate),
+         {:ok, %{"status" => 0}} <- Jason.decode(session_navigate_body) do
+      session_navigate_actions(session_url, url_to_navigate, actions - 1)
+    else
+      error -> error
+    end
+  end
+
+  defp session_navigate_actions(_, _, _), do: :ok
 
   defp desired_capabilities_body(nil),
     do: ~s({"desiredCapabilities":{"browserName": "chrome"}})
